191412ffd260016e2c6979e14ede7e0bd53d967f
Johnny <johnnyturco@me.com>
2022-11-14 09:31:00 -0700

    HTML & JS prototype build-out

---
+++ index.js
@@ -0,0 +1,19 @@
+let cityUrl;
+function searchByCity(){
+  const cityForm = document.querySelector('#search-city-form');
+  cityForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let city = e.target['city-input'].value;
+    cityUrl = `https://api.openbrewerydb.org/breweries?by_city=${city}&per_page=10`
+    // console.log(city);
+    fetch(cityUrl)
+      .then(r => r.json())
+      .then(breweries => {
+        breweries.forEach(brewery => {
+        console.log(brewery.name)
+    })
+  })
+  })
+}
+searchByCity();
