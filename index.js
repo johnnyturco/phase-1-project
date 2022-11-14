5d23f28d3c9973fdcd5c23a0d4185673484658ce
Johnny <johnnyturco@me.com>
2022-11-14 12:33:53 -0700

    put fetch into a function and cleaned up code

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-function searchByCity(){
+const breweryResults = document.querySelector('#brewery-results')
+function searchForm(){
   const cityForm = document.querySelector('#search-city-form');
   cityForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    
+    breweryResults.textContent = "";
+    let cityUrl = `https://api.openbrewerydb.org/breweries?by_city=${e.target['city-input'].value}`
 
-    let city = e.target['city-input'].value;
-    let cityUrl = `https://api.openbrewerydb.org/breweries?by_city=${city}`
+    searchForBreweries(cityUrl);
 
-    fetch(cityUrl)
-      .then(r => r.json())
-      .then(breweries => {
-        const breweryResults = document.querySelector('#brewery-results')
-        breweries.forEach(brewery => {
-        //console.log(brewery.name)
-        const individualBrewery = document.createElement('li')
-        individualBrewery.textContent = brewery.name
-        breweryResults.append(individualBrewery)
-    })
-  })
+    cityForm.reset();
   })
 }
-searchByCity();
+searchForm();
+
+function searchForBreweries(cityUrl){
+  fetch(cityUrl)
+  .then(r => r.json())
+  .then(breweries => {
+    breweries.forEach(brewery => {
+      //console.log(brewery.name)
+      const individualBrewery = document.createElement('li')
+      individualBrewery.textContent = brewery.name
+      breweryResults.append(individualBrewery)
+    })
+  })
+}
